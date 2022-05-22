c18c6716212e5f9a897ba11a3fd6df0e8388cacb
Maneet-K <maneetkaur797@gmail.com>
2022-05-22 12:40:37 +0530

    navbar

---
+++ Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import styles from '../styles/Navbar.module.css';
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callbutton}>
+          <CallOutlinedIcon />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>BUY NOW!</div>
+          <div className={styles.text}>012 333 666</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>HOME PAGE</li>
+          <li className={styles.listItem}>BOOK TYPES</li>
+          <Image src="/img/logo2.png" alt="" width={"120px"} height={"70px"} className={styles.logo}/>
+          <li className={styles.listItem}>BLOGS EVENTS</li>
+          <li className={styles.listItem}>ABOUT CONTACT</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <ShoppingCartOutlinedIcon />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
